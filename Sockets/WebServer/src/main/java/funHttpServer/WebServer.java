9cb62ae9fa0a0183472173191042fbfdb7112101
William deSchryver <archeoavis@gmail.com>
2022-09-01 23:06:14 -0400

    Add multiply error handling

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -194,25 +194,48 @@ class WebServer {
             builder.append("File not found: " + file);
           }
         } else if (request.contains("multiply?")) {
-          // This multiplies two numbers, there is NO error handling, so when
-          // wrong data is given this just crashes
+          Integer num1;
+          Integer num2;
+          boolean goodInput = true;
+          String error = "";
 
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
 
           // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+          try {
+              num1 = Integer.parseInt(query_pairs.get("num1"));
+          } catch (Exception e) {
+              num1 = 1;
+              goodInput = false;
+              error = error+"num1 parameter malformed with error: "+e.getMessage()+"<br>Using default value of 1<br>";
+          }
+          try {
+              num2 = Integer.parseInt(query_pairs.get("num2"));
+          } catch (Exception e) {
+              num2 = 2;
+              goodInput = false;
+              error = error+"num2 parameter malformed with error: "+e.getMessage()+"<br>Using default value of 2<br>";
+          }
 
           // do math
           Integer result = num1 * num2;
 
           // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
+          if (goodInput) {
+              builder.append("HTTP/1.1 200 OK\n");
+              builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append("Result is: " + result);
+          } else {
+              builder.append("HTTP/1.1 200 OK\n");
+              builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append(error);
+              builder.append("\n");
+              builder.append("Result is: " + result);
+          }
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
